8b3cba3c3780b5a9e4c451137b22e3fe2af7357e
Cleyton Lucas Cabral da Silva <Cleytonlucasiurd@gmail.com>
2023-01-22 12:28:31 -0300

    Finalização dos pokemons

--- pages/pokemon/[pokemonId].js
+++ pages/pokemon/[pokemonId].js
@@ -1,4 +1,6 @@
-import { useRouter } from "next/router"
+import Image from "next/image";
+import styles from '../../styles/Pokemon.module.css'
+
 
 export const  getStaticPaths = async () => {
     const maxPokemons = 251;
@@ -29,12 +31,37 @@ export const getStaticProps = async (context) => {
 
 export default function PokemonId({pokemon}){
     
-    const router = useRouter();
-    const PokemonId = router.query.PokemonId
     
     return(
-        <>
-            <h1>{pokemon.name}</h1>
-        </>
+        <div className={styles.pokemon_container}>
+            <h1 className={styles.title}>{pokemon.name}</h1>
+            <Image src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+                width={200} height={200} alt="Pokemon"
+            />
+            <div>
+                <h3>Número:</h3>
+                <p>#{pokemon.id}</p>
+            </div>
+
+            <div>
+                <h3>Tipo:</h3>
+                <div className={styles.types_container}>
+                    {pokemon.types.map((item, index) => (
+                        <span className={`${styles.type} ${styles['type_' + item.type.name]}`} key={index}>{item.type.name}</span>
+                    ))}
+                </div>
+            </div>
+
+            <div className={styles.data_container}>
+                <div className={styles.data_height}>
+                    <h4>Altura</h4>
+                    <p>{pokemon.height * 10} cm</p>
+                </div>
+                <div className={styles.data_weight}>
+                    <h4>Peso:</h4>
+                    <p>{pokemon.weight / 10} kg</p>
+                </div>
+            </div>
+        </div>
     )
 }
