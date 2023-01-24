8a408ac29f8bc31283954abf8b2870543d6f7fd6
Cleyton Lucas Cabral da Silva <Cleytonlucasiurd@gmail.com>
2023-01-24 18:02:30 -0300

    Filtragem por busca feita

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import InputPesquisa from '@/components/InputPesquisa';
+// import InputPesquisa from '@/components/InputPesquisa';
 import styles from '@/styles/Home.module.css'
 import Image from 'next/image';
+import { useState } from 'react';
 import Card from '../components/Card'
 
 
@@ -20,7 +21,12 @@ export async function getStaticProps(){
 }
 
 
+
 export default function Home({pokemons}) {  
+
+  const [event, setEvent] = useState('')
+
+  const filter = pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(event.toLowerCase()))
 
   return (
     <>
@@ -29,10 +35,20 @@ export default function Home({pokemons}) {
         <Image src='/images/pokeball.png' width={50} height={50} alt="Pokedex" />
       </div>
 
-        <InputPesquisa src='/images/iconSearch.png' width={20} height={20} placeholder="Busca Pokemon (ainda não está funcioanando)" />
-      
+        {/* <InputPesquisa src='/images/iconSearch.png' width={20} height={20} placeholder="Busca Pokemon (ainda não está funcioanando)" /> */}
+        
+        <div className={styles.container_pesquisa}>
+                <input  
+                    onChange={(e) => setEvent(e.target.value)} 
+                    value={event} 
+                    type="search" 
+                    placeholder="Busca Pokemon (ainda não está funcioanando)"
+                    />
+                <Image src={'/images/iconSearch.png'} width={20} height={20} />
+            </div>
+
       (<div className={styles.pokemon_container}>
-        {pokemons.map(pokemon => (
+        {filter.map(pokemon => (
           <Card key={pokemon.id} pokemon={pokemon} />
         ))}
       </div>)
