e69cc3186a2a6120f5c81b069197086a7779de5c
CleytonLu <cleytonlucasiurd@gmail.com>
2024-03-09 00:28:44 -0300

    Ajuste em categorias page

--- pages/pageCategoria.js
+++ pages/pageCategoria.js
@@ -1,31 +1,35 @@
-import Link from 'next/link';
-import styles from '../styles/IndexCategoria.module.css'
-
+import Link from "next/link";
+import styles from "../styles/IndexCategoria.module.css";
 
 export async function getStaticProps() {
+  const res = await fetch(`https://pokeapi.co/api/v2/type/`);
+  const data = await res.json();
 
-    const res = await fetch(`https://pokeapi.co/api/v2/type/`)
-    const data = await res.json()
+  data.results.forEach((item, index) => {
+    item.id = index + 1;
+  });
 
-    data.results.forEach((item, index) => {
-        item.id = index + 1
-    });
-
-    return { props: { tipos: data.results } }
-
+  return { props: { tipos: data.results } };
 }
 
-export default function PageCategoria({tipos}) {
+export default function PageCategoria({ tipos }) {
+  console.log(tipos);
 
-    console.log(tipos)
-
-    return (
-        <div className={styles.CardTipo}>
-            {tipos.map((tipo) => (
-                <Link href={`/categoria/${tipo.id}`} key={tipo.id} className={styles.tipo}>
-                    <span className={`${styles.tipo} ${styles['type_' + tipo.name]}`}>{tipo.name}</span></Link>
-            ))}
-
-        </div>
-    )
+  return (
+    <div className={styles.CardTipo}>
+      {tipos
+        .map((tipo) => (
+          <Link
+            href={`/categoria/${tipo.id}`}
+            key={tipo.id}
+            className={styles.tipo}
+          >
+            <span className={`${styles.tipo} ${styles["type_" + tipo.name]}`}>
+              {tipo.name}
+            </span>
+          </Link>
+        ))
+        .slice(0, 16)}
+    </div>
+  );
 }
